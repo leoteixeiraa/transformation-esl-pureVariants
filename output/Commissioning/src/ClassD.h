8bbc2fd6351d0d90275e7e27a4d3071deb3a07f9
Leonardo Teixeira <leonardo.teixeira@estudante.ufscar.br>
2025-11-20 14:47:30 -0300

    Refactor: Remove unused classes and documentation files
    - Deleted ClassA, ClassB, ClassC, ClassD, and ClassE header files as they are no longer needed.
    - Removed associated implementation details and the main program that instantiated these classes.
    - Deleted documentation files (docu_xhtml.xml and readme.txt) that are no longer relevant.
    - Cleaned up the Makefile and configuration files as they pertain to the removed classes.

--- output/Commissioning/src/ClassD.h
+++
@@ -1,5 +0,0 @@
-#ifndef __pv_guard_igbCHA9vaEryaCaLP_iVmLwtvpqKsIs91Ar
-#define __pv_guard_igbCHA9vaEryaCaLP_iVmLwtvpqKsIs91Ar
-#include "C:/purevariants_repo/Transformation ESL/output/Commissioning/src/ClassB.h"
-typedef ::ClassB ClassD;
-#endif
